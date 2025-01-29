21cfbcea575593f853527e2bd9f0a5e5bd11cd78
Mason Judd <masonjudd@mail.weber.edu>
2025-01-29 15:18:29 -0700

    shape calculator

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,3 +1,4 @@
+// Main Page
 function Default() {
     document.getElementById('mode').innerHTML = "Current Mode: Default";
     document.getElementById('pageBody').style = "background-color: #3b3b3b";
@@ -27,4 +28,23 @@ function Light() {
     document.getElementById('mainParagraph').style = "background-color: gray";
     document.getElementById('mainParagraph').style.color = "black";
     document.getElementById('subParagraph').style = "background-color: white";
+}
+
+// Shape Calculator
+function circumference() {
+    let r = parseFloat(document.getElementById('radius1').value);
+    let result = 2 * (Math.PI) * r;
+    document.getElementById("circumferenceResult").innerHTML = result;
+}
+
+function area() {
+    let r = parseFloat(document.getElementById('radius1').value);
+    let result = (Math.PI) * ((r)**2);
+    document.getElementById("areaResult").innerHTML = result;
+}
+
+function volume() {
+    let r = parseFloat(document.getElementById('radius2').value);
+    let result = (4/3) * (Math.PI) * ((r)**3);
+    document.getElementById("volumeResult").innerHTML = result;
 }
